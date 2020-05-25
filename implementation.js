9d8750c6c3028bfd8aa11efb130448d3fc7b22a5
Jordan Harband <ljharb@gmail.com>
2020-05-25 00:35:47 -0700

    [patch] spec change; `errors` is now an own data instead of a prototype accessor
    See https://github.com/tc39/proposal-promise-any/pull/64

--- implementation.js
+++ implementation.js
@@ -1,20 +1,17 @@
 'use strict';
 
 var AdvanceStringIndex = require('es-abstract/2019/AdvanceStringIndex');
+var CreateDataPropertyOrThrow = require('es-abstract/2019/CreateDataPropertyOrThrow');
 var GetMethod = require('es-abstract/2019/GetMethod');
 var IsArray = require('es-abstract/2019/IsArray');
 var IterableToList = require('es-abstract/2019/IterableToList');
 var OrdinarySetPrototypeOf = require('es-abstract/2019/OrdinarySetPrototypeOf');
 var Type = require('es-abstract/2019/Type');
 var GetIntrinsic = require('es-abstract/GetIntrinsic');
-var callBound = require('es-abstract/helpers/callBound');
 var getIteratorMethod = require('es-abstract/helpers/getIteratorMethod');
 var define = require('define-properties');
 
 var $Error = GetIntrinsic('%Error%');
-var $slice = callBound('Array.prototype.slice');
-
-var SLOT = require('internal-slot');
 
 // eslint-disable-next-line func-style
 function AggregateError(errors, message) {
@@ -27,11 +24,7 @@ function AggregateError(errors, message) {
 	var error = new $Error(message);
 	OrdinarySetPrototypeOf(error, proto); // eslint-disable-line no-use-before-define
 	delete error.constructor;
-	if (define.supportsDescriptors) {
-		SLOT.set(error, '[[AggregateErrors]]', errorsList);
-	} else {
-		error.errors = errorsList; // back compat fallback
-	}
+	CreateDataPropertyOrThrow(error, 'errors', errorsList);
 	return error;
 }
 var proto = AggregateError.prototype;
@@ -41,17 +34,6 @@ define(AggregateError.prototype, {
 	message: '',
 	name: 'AggregateError'
 });
-
-if (define.supportsDescriptors) {
-	Object.defineProperty(AggregateError.prototype, 'errors', {
-		configurable: true,
-		enumerable: false,
-		get: function () {
-			SLOT.assert(this, '[[AggregateErrors]]');
-			return $slice(SLOT.get(this, '[[AggregateErrors]]'));
-		}
-	});
-}
 
 OrdinarySetPrototypeOf(AggregateError.prototype, Error.prototype);
 
